a314e9f97f268cd91ca39f4f95449bed64caa668
JJW-27 <josephjonwilliams@outlook.com>
2022-11-17 10:23:05 +0000

    adds happy path for when there are 0 comments as well as more than 0 comments

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -18,7 +18,13 @@ exports.selectReviews = () => {
 
 exports.selectReviewById = review_id => {
   return db
-    .query(`SELECT * FROM reviews WHERE review_id = $1;`, [review_id])
+    .query(`SELECT reviews.review_id, title, category, designer, owner, review_body, review_img_url, reviews.votes, reviews.created_at, COUNT(comments.review_id) ::INT AS comment_count
+    FROM reviews
+    JOIN users ON reviews.owner = users.username 
+    LEFT JOIN comments ON reviews.review_id = comments.review_id
+    WHERE reviews.review_id = $1
+    GROUP BY reviews.review_id, users.username
+    `, [review_id])
     .then(review => {
       if (review.rows.length === 1) {
         return review.rows;
